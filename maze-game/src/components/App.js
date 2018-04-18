76c9413176bca6f199e4d326a05381b7abd083ff
Micah <mickey4sure2k7@gmail.com>
2018-04-18 11:39:03 +0100

    init game and create game board

--- maze-game/src/components/App.js
+++ maze-game/src/components/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
+// import './index.css';
 
-class App extends React.Component {
+class Square extends React.Component {
     render() {
-        return <h1>Hello React!</h1>
+        return (
+            <div className="square" style={{width: this.props.boxWidth}}>
+              {this.props.content}
+            </div>
+        );
     }
 }
 
-export default App;
+class Board extends React.Component {
+
+    componentWillMount() {
+        this.rows = parseInt(prompt('How many rows do you want?'), 10);
+        this.cols = parseInt(prompt('How many columns do you want?'), 10);
+        this.cells = this.row * this.cols;
+        this.size = 500 / this.cols;
+    }
+
+    render() {
+        let totalCells = [];
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.cols; j++) {
+                totalCells.push(<Square content={`${i}${j}`} boxWidth={`${this.size}px`}/>);
+            }
+        }
+
+        return (
+            <div className="board">
+              {totalCells}
+            </div>
+        )
+    }
+}
+
+export default Board;
