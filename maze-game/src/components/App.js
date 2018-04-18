68a2ae213b3f027b2b3e72ab53287b05ed0b6724
Micah <mickey4sure2k7@gmail.com>
2018-04-18 12:07:43 +0100

    Add new components

--- maze-game/src/components/App.js
+++ maze-game/src/components/App.js
@@ -1,6 +1,30 @@
 import React from 'react';
 // import './index.css';
 
+
+class Player extends React.Component {
+    render() {
+        return (
+          <div className="square" 
+               style={{width: this.props.boxWidth }}>
+            {this.props.content}
+          </div>
+        )
+    }
+}
+
+class Target extends React.Component {
+    render() {
+        return (
+            <div className="square" 
+                 style={{width: this.props.boxWidth }}>
+                 {this.props.content}
+            </div>
+        )
+    }
+}
+
+
 class Square extends React.Component {
     render() {
         return (
@@ -24,7 +48,11 @@ class Board extends React.Component {
         let totalCells = [];
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.cols; j++) {
-                totalCells.push(<Square content={`${i}${j}`} boxWidth={`${this.size}px`}/>);
+                if (i === 2 && j === 2) {
+                    totalCells.push(<Player content="P" boxWidth={`${this.size}px`} key = "22"/>);
+                } else {
+                    totalCells.push(<Square content={`${i}${j}`} boxWidth={`${this.size}px`} key = {`${i}${j}`.toString()}/>);
+                }
             }
         }
 
@@ -35,5 +63,6 @@ class Board extends React.Component {
         )
     }
 }
+
 
 export default Board;
