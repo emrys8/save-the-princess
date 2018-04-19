e78cf3ee880f5c32f2c044473c069f2ad2d10d5b
Micah <mickey4sure2k7@gmail.com>
2018-04-19 02:50:19 +0100

    Add dynamic maze cells generation

--- maze-game/src/components/App.js
+++ maze-game/src/components/App.js
@@ -28,8 +28,9 @@ class Target extends React.Component {
 class Cell extends React.Component {
     render() {
         return (
-            <div className="square" style={{width: this.props.boxWidth}}>
-              {this.props.content}
+            <div className="cell" 
+                 style={this.props.cellDim}>
+                 {this.props.content}
             </div>
         );
     }
@@ -37,28 +38,52 @@ class Cell extends React.Component {
 
 class Maze extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            cells: [null]
+        }
+        this.maxMazeSize = 500; // px
+    }
+    
+
     componentWillMount() {
-        this.rows = parseInt(prompt('Please enter box width: '), 10);
-        this.cols = parseInt(prompt('Please enter box height: '), 10);
-        this.cells = this.row * this.cols;
-        this.size = 500 / this.cols;
+        this.rows = parseInt(prompt('Enter box width: '), 10);
+        this.cols = parseInt(prompt('Enter enter box height: '), 10);
+        this.cells = this.rows * this.cols;
+        this.size = this.maxMazeSize / this.cols;
+
+        this.setState({
+            cells: Array(this.cells).fill(null),
+        })
+    }
+
+    componentDidMount() {
+
+    }
+
+    renderCell(content) {
+        const size = this.size;
+
+        // an equal width, and equal height cell
+        return <Cell 
+                   content={content} 
+                   cellDim = {{width: `${size}px`, height: `${size}px`}} 
+                   key = {content} 
+                />
     }
 
     render() {
-        let totalCells = [];
-        for (let i = 0; i < this.rows; i++) {
-            for (let j = 0; j < this.cols; j++) {
-                if (i === 2 && j === 2) {
-                    totalCells.push(<Player content="P" boxWidth={`${this.size}px`} key = "22"/>);
-                } else {
-                    totalCells.push(<Cell content={`${i}${j}`} boxWidth={`${this.size}px`} key = {`${i}${j}`.toString()}/>);
-                }
-            }
-        }
+
+        const cells = this.state.cells;
+        const allCells = [];
+        cells.forEach(value => {
+            allCells.push(this.renderCell(value));
+        });
 
         return (
-            <div className="board">
-              {totalCells}
+            <div className="maze">
+              {allCells}
             </div>
         )
     }
